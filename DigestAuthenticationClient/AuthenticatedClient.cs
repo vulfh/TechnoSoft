c73aaf1f2fa6fcd96c7566915a92ea6fe6c5e1d2
vulfh <Miyamoto1977!>
2022-03-19 19:24:06 +0200

    initial commit

---
+++ DigestAuthenticationClient/AuthenticatedClient.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using RestSharp.Authenticators.Digest;
+using RestSharp.Serializers.Utf8Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DigestAuthenticationClient
+{
+    public class AuthenticatedClient:IDisposable
+    {
+        public string UserName { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string BaseUrl { get; private set; }
+
+        private RestClient _client;
+        public AuthenticatedClient(string baseUrl,string username,string password)
+        {
+            ServicePointManager.ServerCertificateValidationCallback +=
+                    (sender, certificate, chain, sslPolicyErrors) => true;
+            UserName = username;
+            Password = password;
+            BaseUrl = baseUrl;
+            _client = new RestClient(baseUrl)
+            {
+                Authenticator = new DigestAuthenticator(string.IsNullOrWhiteSpace(username) ? UserName : username,
+                                                       string.IsNullOrWhiteSpace(password) ? Password : password)
+            };
+
+
+        }
+        public async Task<string> PostJson<T>(string resource,T payload) where T:class
+        {
+            try
+            {
+                var request = new RestRequest(resource, Method.Post);
+                request.RequestFormat = DataFormat.Json;
+                var restRequest = request.AddJsonBody(payload);
+                var response = await _client.PostAsync(restRequest);
+                return response.Content;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed post {payload} to {resource}.", ex);
+            }
+
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _client.Dispose();
+            }
+            finally { }
+        }
+    }
+}
---
+++ TestDigestAuthentication/Program.cs
@@ -0,0 +1,17 @@
+﻿using DigestAuthenticationClient;
+using System;
+using System.Threading.Tasks;
+
+namespace TestDigestAuthentication
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            AuthenticatedClient client = new AuthenticatedClient("http://localhost/api", "user", "password");
+            var response = await client.PostJson("upload",new { Foo = "foo", Bar = "bar", Quiz = "Quiz" });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
